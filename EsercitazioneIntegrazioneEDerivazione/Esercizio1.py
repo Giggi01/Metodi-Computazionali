07d64819639525e36206d2dc3c7f8a730f8f1bfe
Giggi01 <lg010026@infolab01>
2022-10-26 17:14:09 +0200

    Agg. esercizio 1

--- EsercitazioneIntegrazioneEDerivazione/Esercizio1.py
+++ EsercitazioneIntegrazioneEDerivazione/Esercizio1.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import scipy as sc
+from scipy import integrate
 
 #Lettura file 
 
@@ -13,9 +15,34 @@ ax = df_fromfile['t']
 
 ay = df_fromfile['v']
 
-
-plt.plot(ax,ay)
+fig = plt.figure()
+plt.plot(ax, ay ,'o-', color = 'limegreen', label = 'Velocita')
 plt.xlabel('Time')
-plt.ylabel('Space')
-
+plt.ylabel('Velocity')
+fig.suptitle('Grafico Velocita-Tempo', fontsize = 20)
+plt.legend()
+plt.grid(True)
 plt.show()
+
+#Calcolo dell'integrale su tutto il tempo
+
+distanza_tot = sc.integrate.simpson(ax, ay)
+print('La distanza totale percorsa è: ', distanza_tot)
+
+#Definisco l'array delle distanze calcolate con il metodo scipy.integrate.simpson tramite un ciclo for
+
+distanza = np.array([])
+for i in range(len(ax)):
+    distanza = np.append(distanza, sc.integrate.simpson(ay[:i+1], ax[:i+1]))
+#print(distanza)
+
+#Creazione del grafico distanza-tempo
+fig = plt.figure()
+plt.plot(distanza, ax, 'o-', color = 'red', label = 'Distanza')
+plt.xlabel('Time')
+plt.ylabel('Distance')
+fig.suptitle('Grafico Distanza-Tempo', fontsize = 20)
+plt.legend()
+plt.grid(True)
+plt.show()
+
