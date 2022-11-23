55cca28404c0ef08d578e22acde096516aed208d
Giggi01 <lg010026@infolab01>
2022-11-23 17:43:53 +0100

    Implementare parte 1 di 1 e fare parte 2 e 3

---
+++ EsercitazioneMonteCarlo/Esercizio1.py
@@ -0,0 +1,39 @@
+import numpy as np
+import scipy as sc
+import matplotlib.pyplot as plt
+import math
+
+scalar = 2.0
+
+#Funzione del random walker
+
+def runwalk(s, passi):
+    walkx = [0.0]
+    walky = [0.0]
+    for i in range(0, passi-1, 1):
+        phi = np.random.uniform(low = 0.0,  high = 2*math.pi, size = 1)
+        dx = s*math.cos(phi)
+        dy = s*math.sin(phi)
+        walkx.append(walkx[i]+dx)
+        walky.append(walky[i]+dy)
+    return walkx, walky
+
+#Grafico del random walker
+
+random = runwalk(scalar, 1000)
+randomx = random[0]
+randomy = random[1]
+
+print(len(randomx))
+
+
+for i in range(0, 1000, 1):
+    random = runwalk(scalar, 1000)
+    randomx = random[0]
+    randomy = random[1]
+    plt.plot(randomx[9], randomy[9], 'o', color = 'red', label = '10imo passo')
+    plt.plot(randomx[99], randomy[99], 'o', color = 'blue', label = '100imo passo')
+    plt.plot(randomx[999], randomy[999], 'o', color = 'green', label = '1000imo passo')
+plt.xlabel('Passi X')
+plt.ylabel('Passi Y')
+plt.show()
