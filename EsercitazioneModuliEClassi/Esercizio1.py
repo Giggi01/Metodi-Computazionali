567546fb2ae21007578e684e30b92d1da31cf0b8
Giggi01 <lg010026@infolab01>
2022-12-14 17:39:20 +0100

    Agg. esercitazione moduli e classi

--- EsercitazioneModuliEClassi/Esercizio1.py
+++ EsercitazioneModuliEClassi/Esercizio1.py
@@ -4,14 +4,6 @@ import numpy as np
 import math
 import reco as rc
 
-#Definizione di una funzione che crea un array di reco.Hit
-
-def arrrecohit(datimodulo):
-    arrRHit = np.array([])
-    for i in range(len(datimodulo['hit_time'])):
-        arrRHit = np.append(arrRHit, rc.Hit(datimodulo['mod_id'][i], datimodulo['det_id'][i], datimodulo['hit_time'][i]))
-    return arrRHit
-
 ###PASSO 1###
 
 #Lettura di tutti i file (moduli)
@@ -24,23 +16,31 @@ datimodulo3 = pd.read_csv('hit_times_M3.csv')
 #Istrogramma dei tempi per il modulo 0
 
 timemodulo0 = datimodulo0['hit_time'].values
-print((len(timemodulo0)))
-plt.hist(timemodulo0, bins = 100)
-plt.xlabel('$times$')
+plt.hist(timemodulo0, bins = 45)
 plt.ylabel('$Events$')
+plt.grid(True)
 plt.show()
 
 #Istogramma delle differenze dei tempi fra Hit consecutivi del modulo 0
 
 deltat = np.diff(timemodulo0)
-mask0 = deltat > 0
-logdt = np.log(deltat[mask0])
+mask0 = deltat > 0.0
+logdt = np.log10(deltat[mask0])
 plt.hist(logdt, bins = 100)
 plt.xlabel('$\Delta$time')
 plt.ylabel('$Events$')
+plt.grid(True)
 plt.show()
 
 ###PASSO 3###
+
+#Definizione di una funzione che crea un array di reco.Hit
+
+def arrrecohit(datimodulo):
+    arrRHit = np.array([])
+    for i in range(len(datimodulo['hit_time'])):
+        arrRHit = np.append(arrRHit, rc.Hit(datimodulo['mod_id'][i], datimodulo['det_id'][i], datimodulo['hit_time'][i]))
+    return arrRHit
 
 #Creo un array di reco.Hit per ogni modulo (4), richimando la funzione arrrechit per ogni modulo
 
@@ -57,6 +57,26 @@ arrRH = np.append(arrRH, arrRH1)
 arrRH = np.append(arrRH, arrRH2)
 arrRH = np.append(arrRH, arrRH3)
 
-#Faccio un bubble sort per mettere in ordine cronologico tutti gli array reco.Hit
+#Utilizzo il metodo numpy.sort per ordinare cronologicamente l'array di tutti gli Hit
 
+sortarrRH = np.sort(arrRH)
 
+#Calcolo i deltat per Hit consecutivi
+
+arrRHdeltat = np.diff(sortarrRH)
+arrRHdeltat = arrRHdeltat.astype(int)
+mask0 = arrRHdeltat > 0.0
+loghitdt = np.log10(arrRHdeltat[mask0])
+
+#Creo l'istogramma per dt di Hit consecutivi
+
+plt.hist(loghitdt, 60)
+plt.xlabel('$log_10$$\Delta$time')
+plt.ylabel('$Events$')
+#plt.xscale('log')
+plt.grid(True)
+plt.show()
+
+###PASSO 4###
+
+#
--- EsercitazioneModuliEClassi/reco.py
+++ EsercitazioneModuliEClassi/reco.py
@@ -16,3 +16,30 @@ class Hit():
     def __eq__(self, other):
         return self.tsriv == other.tsriv
 
+    def __sub__(self, other):
+        return self.tsriv - other.tsriv
+
+    def __repr__(self):
+        return '({0}, {1}, {2})'.format(self.idmod, self.idsens, self.tsriv)
+
+class Evento():
+
+    def __init__(self):
+        self.nhit = 0
+        self.tstart = 0
+        self.tstop = 0
+        self.tspan = 0
+        self.hits = 0
+
+    def __gt __(self, tstart):
+        return self.tstart > other.tstart
+
+    def AggHit (self, hit):
+        self.nhit += 1
+        self.hits = np.append(hits, hit)
+        self.tstop = hit.tsriv
+        if nhit == 1:
+            tstart = hit.tsriv
+        self.tspan = tstop - tstart
+        
+    
