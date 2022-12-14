38e5ee980ddb61a700f76c2edaa0197abdb109c2
Giggi01 <luigiguerrini2001@gmail.com>
2022-12-14 14:39:55 +0100

    Agg. esercitazione moduli e classi

--- EsercitazioneModuliEClassi/Esercizio1.py
+++ EsercitazioneModuliEClassi/Esercizio1.py
@@ -4,9 +4,22 @@ import numpy as np
 import math
 import reco as rc
 
-#Lettura del file (Modulo 0)
+#Definizione di una funzione che crea un array di reco.Hit
+
+def arrrecohit(datimodulo):
+    arrRHit = np.array([])
+    for i in range(len(datimodulo['hit_time'])):
+        arrRHit = np.append(arrRHit, rc.Hit(datimodulo['mod_id'][i], datimodulo['det_id'][i], datimodulo['hit_time'][i]))
+    return arrRHit
+
+###PASSO 1###
+
+#Lettura di tutti i file (moduli)
 
 datimodulo0 = pd.read_csv('hit_times_M0.csv')
+datimodulo1 = pd.read_csv('hit_times_M1.csv')
+datimodulo2 = pd.read_csv('hit_times_M2.csv')
+datimodulo3 = pd.read_csv('hit_times_M3.csv')
 
 #Istrogramma dei tempi per il modulo 0
 
@@ -14,6 +27,7 @@ timemodulo0 = datimodulo0['hit_time'].values
 print((len(timemodulo0)))
 plt.hist(timemodulo0, bins = 100)
 plt.xlabel('$times$')
+plt.ylabel('$Events$')
 plt.show()
 
 #Istogramma delle differenze dei tempi fra Hit consecutivi del modulo 0
@@ -23,6 +37,26 @@ mask0 = deltat > 0
 logdt = np.log(deltat[mask0])
 plt.hist(logdt, bins = 100)
 plt.xlabel('$\Delta$time')
+plt.ylabel('$Events$')
 plt.show()
+
+###PASSO 3###
+
+#Creo un array di reco.Hit per ogni modulo (4), richimando la funzione arrrechit per ogni modulo
+
+arrRH0 = arrrecohit(datimodulo0)
+arrRH1 = arrrecohit(datimodulo1)
+arrRH2 = arrrecohit(datimodulo2)
+arrRH3 = arrrecohit(datimodulo3)
+
+#Fondo tutti e 4 gli array in uno unico
+
+arrRH = np.array([])
+arrRH = np.append(arrRH, arrRH0)
+arrRH = np.append(arrRH, arrRH1)
+arrRH = np.append(arrRH, arrRH2)
+arrRH = np.append(arrRH, arrRH3)
+
+#Faccio un bubble sort per mettere in ordine cronologico tutti gli array reco.Hit
 
 
--- EsercitazioneModuliEClassi/reco.py
+++ EsercitazioneModuliEClassi/reco.py
@@ -1,3 +1,5 @@
+###PASSO 2###
+
 class Hit():
 
     def __init__(self, idmod, idsens, tsriv):
@@ -6,25 +8,11 @@ class Hit():
         self.tsriv = tsriv
 
     def __lt__(self, other):
-        retunr self.tsriv < other.tsriv
+        return self.tsriv < other.tsriv
 
     def __gt__(self, other):
-        retunr self.tsriv > other.tsriv
+        return self.tsriv > other.tsriv
 
-        
-class Event():
+    def __eq__(self, other):
+        return self.tsriv == other.tsriv
 
-    def __init__(self, hit):
-        self.hit = hit
-
-    def arrhit(self,hit):
-        arrhit = np.arr([])
-        arrhit = np.append(arrhit, self.hit)
-        dt = arrhit[len(arrhit)].tsriv - arrhit[0].tsriv
-        return arrhit, arrhit[0].tsriv, arrhit[len(arrhit)].tsriv, len(arrhit), 
-        
-        
-        
-
-    
-    
